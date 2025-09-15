5582bc6e112fdc299814fd63bb556e11b7283860
Mac Erskine <mclean.erskine@aspiraconnect.com>
2025-09-15 10:08:18 -0700

    refactor: removes lint packages

--- init.lua
+++ init.lua
@@ -176,7 +176,6 @@ if not vim.g.vscode then
     --
     -- require 'kickstart.plugins.debug',
     -- require 'kickstart.plugins.indent_line',
-    -- require 'kickstart.plugins.lint',
     require 'kickstart.plugins.autopairs',
     require 'kickstart.plugins.neo-tree',
     require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps

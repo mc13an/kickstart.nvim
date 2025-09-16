8620eb7eed3f21198716d275f2bfd4ba394bf2a3
Mac Erskine <mclean.erskine@aspiraconnect.com>
2025-09-16 08:16:54 -0700

    refactor: sets up none-ls for diagnostics and formatting

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -2,15 +2,41 @@ return {
   -- none-ls
   'nvimtools/none-ls.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvimtools/none-ls-extras.nvim', 'davidmh/cspell.nvim' },
   config = function()
     local null_ls = require 'null-ls'
+    local cspell = require 'cspell'
+
     null_ls.setup {
+      debug = true, -- Enable debug mode to see what's happening
       sources = {
-        null_ls.builtins.formatting.biome,
-        null_ls.builtins.diagnostics.codespell,
-        null_ls.builtins.formatting.codespell,
+        -- Formatting
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.stylua,
+        -- Diagnostics
+        require 'none-ls.diagnostics.eslint_d',
+        -- Cspell spell checking
+        cspell.diagnostics.with {
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity = vim.diagnostic.severity.HINT
+          end,
+        },
+        -- Code Actions
+        require 'none-ls.code_actions.eslint_d',
+        cspell.code_actions,
       },
     }
+
+    vim.keymap.set('n', '<leader>fbb', vim.lsp.buf.format, {})
+
+    -- Format on save
+    local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = augroup,
+      pattern = '*',
+      callback = function()
+        vim.lsp.buf.format { async = false }
+      end,
+    })
   end,
 }
